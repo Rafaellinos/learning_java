989ff387632f980da0d50cc13ffb96c9a5664341
Rafael Lino <rafael.veloso.lino@hotmail.com>
2023-07-10 21:47:51 -0300

    feat: add publicador eventos e ajuste test

--- 1964-java-domain-driven-design-projeto_inicial/1964-java-domain-driven-design-projeto_inicial/src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
+++ 1964-java-domain-driven-design-projeto_inicial/1964-java-domain-driven-design-projeto_inicial/src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
@@ -2,6 +2,8 @@ package br.com.alura.escola;
 
 import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAluno;
 import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAlunoDto;
+import br.com.alura.escola.dominio.PublicadorEventos;
+import br.com.alura.escola.dominio.aluno.LogAlunoMatriculado;
 import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
 
 public class MatricularAlunoPorLinhaDeComando {
@@ -10,8 +12,15 @@ public class MatricularAlunoPorLinhaDeComando {
 		String nome = "Fulano da Silva";
 		String cpf = "123.456.789-00";
 		String email = "fulano@email.com";
-		
-		MatricularAluno matricular = new MatricularAluno(new RepositorioDeAlunosEmMemoria());
+
+		PublicadorEventos publicadorEventos = new PublicadorEventos();
+		publicadorEventos.adicionar(new LogAlunoMatriculado());
+
+		MatricularAluno matricular = new MatricularAluno(
+				new RepositorioDeAlunosEmMemoria(),
+				publicadorEventos
+				);
+
 		matricular.executa(
 				new MatricularAlunoDto(nome, 
 						cpf, 
--- 1964-java-domain-driven-design-projeto_inicial/1964-java-domain-driven-design-projeto_inicial/src/main/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAluno.java
+++ 1964-java-domain-driven-design-projeto_inicial/1964-java-domain-driven-design-projeto_inicial/src/main/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -1,20 +1,29 @@
 package br.com.alura.escola.aplicacao.aluno.matricular;
 
+import br.com.alura.escola.dominio.PublicadorEventos;
 import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.AlunoMatriculado;
 import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
 
 public class MatricularAluno {
 	
 	private final RepositorioDeAlunos repositorio;
+	private final PublicadorEventos publicadorEventos;
 
-	public MatricularAluno(RepositorioDeAlunos repositorio) {
+	public MatricularAluno(
+			RepositorioDeAlunos repositorio,
+			PublicadorEventos publicadorEventos
+	) {
 		this.repositorio = repositorio;
+		this.publicadorEventos = publicadorEventos;
 	}
 	
 	// COMMAND
 	public void executa(MatricularAlunoDto dados) {
 		Aluno novo = dados.criarAluno();
 		repositorio.matricular(novo);
+		AlunoMatriculado evento = new AlunoMatriculado(novo.getCpf());
+		publicadorEventos.publicar(evento);
 	}
 
 }
